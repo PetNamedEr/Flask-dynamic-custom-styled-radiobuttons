fc88e250743b564e48948404c9f60cc3b6dbf031
PetNamedEr <skogberg.t.peter@student.uta.figit config --global user.emailgit config --global user.email skogberg.t.peter@student.uta.figit config --global user.email skogberg.t.peter@student.uta.fi>
2018-06-07 15:26:35 +0300

    Added input of new choices

--- static/script/CustomRadioButtons.js
+++ static/script/CustomRadioButtons.js
@@ -2,6 +2,8 @@ function setInitValueOnPageLoad()
 {
   var radioButtonChoices = document.getElementsByName('usersChoice');
   radioButtonChoices[radioButtonChoices.length - 1].checked = 1;
+
+  setAnswerValue(radioButtonChoices.length - 1)
 }
 
 function setAnswerValue(userSelectedRadioButtonIndex)
